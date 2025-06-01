0d9b0962c5593719b5101448389a1b18d50cb9d8
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2025-06-01 19:58:27 +0200

    Refactored WinMain to allocate a console and redirect the IO

--- mosaic/include/mosaic/entry_point.hpp
+++ mosaic/include/mosaic/entry_point.hpp
@@ -34,12 +34,37 @@ int runApp(Args&&... args)
 
 #if defined(MOSAIC_PLATFORM_WINDOWS)
 
+#include <windows.h>
+
+#if defined(MOSAIC_DEBUG_BUILD) || defined(MOSAIC_DEV_BUILD)
+
+#define MOSAIC_ENTRY_POINT(AppType, ...)                                           \
+    int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, \
+                       _In_ PSTR lpCmdLine, _In_ int nCmdShow)                     \
+    {                                                                              \
+        AllocConsole();                                                            \
+                                                                                   \
+        FILE* fp;                                                                  \
+        freopen_s(&fp, "CONOUT$", "w", stdout);                                    \
+        freopen_s(&fp, "CONOUT$", "w", stderr);                                    \
+        freopen_s(&fp, "CONIN$", "r", stdin);                                      \
+        std::ios::sync_with_stdio(true);                                           \
+                                                                                   \
+        return mosaic::runApp<AppType>(__VA_ARGS__);                               \
+                                                                                   \
+        FreeConsole();                                                             \
+    }
+
+#else
+
 #define MOSAIC_ENTRY_POINT(AppType, ...)                                           \
     int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, \
                        _In_ PSTR lpCmdLine, _In_ int nCmdShow)                     \
     {                                                                              \
         return mosaic::runApp<AppType>(__VA_ARGS__);                               \
     }
+
+#endif
 
 #elif defined(MOSAIC_PLATFORM_LINUX) || defined(MOSAIC_PLATFORM_MACOS) || \
     defined(MOSAIC_PLATFORM_EMSCRIPTEN)
